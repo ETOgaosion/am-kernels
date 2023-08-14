d1cde96d2204dfb6b02260a393145a708bf64e4a
Blue Space <gaoziyuan19@mails.ucas.ac.cn>
2023-08-14 22:32:00 +0800

    finish pa2-2

--- tests/cpu-tests/tests/hello-str.c
+++ tests/cpu-tests/tests/hello-str.c
@@ -5,12 +5,15 @@ char buf[128];
 int main() {
 	sprintf(buf, "%s", "Hello world!\n");
 	check(strcmp(buf, "Hello world!\n") == 0);
+	memset(buf, 0, 128);
 
 	sprintf(buf, "%d + %d = %d\n", 1, 1, 2);
 	check(strcmp(buf, "1 + 1 = 2\n") == 0);
+	memset(buf, 0, 128);
 
 	sprintf(buf, "%d + %d = %d\n", 2, 10, 12);
 	check(strcmp(buf, "2 + 10 = 12\n") == 0);
+	memset(buf, 0, 128);
 
 	return 0;
 }
